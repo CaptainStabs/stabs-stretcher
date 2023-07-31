be2a7a2a8cc4e2133214e2a4192eb944656fe3b4
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-07-31 16:17:00 -0400

    Add customizable positions for different vehicles

--- config.lua
+++ config.lua
@@ -6,7 +6,15 @@ Config.Vehicles = {
     'yas1',
     'yas2',
     'yas3',
-    'ambo'
+    'HEMS1'
+}
+
+Config.VehiclesPos = {
+    ['yas1'] = {x = 0.3, y = -1.1, z = -0.5},
+    ['yas2'] = {x = 0.3, y = -1.1, z = -0.5},
+    ['yas3'] = {x = 0.3, y = -1.1, z = -0.5},
+    ['HEMS1'] = {x = -0.3, y = -1.5, z = -0.8}
 }
 
 Config.StretcherModel = 'stretcher'
+Config.BackBoard = 'combicarrier2'
