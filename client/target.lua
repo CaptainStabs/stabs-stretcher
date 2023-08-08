be370fa905b3971b71f10a73293b6938d175a51c
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2023-08-08 16:01:26 -0400

    Update target.lua

--- client/target.lua
+++ client/target.lua
@@ -16,6 +16,7 @@ Citizen.CreateThread(function()
                 type = "client",
                 event = "stretcher:getonstretcher",
                 label = "Lay on stretcher",
+                icon = "fas fa-stretcher",
             }
         },
         distance = 10,
@@ -47,18 +48,10 @@ Citizen.CreateThread(function()
             num = 3, -- This is the position number of your option in the list of options in the qb-target context menu (OPTIONAL)
             type = "client", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
             event = "stretcher:client:PlaceOnStretcher", -- This is the event it will trigger on click, this can be a client event, server event, command or qbcore registered command, NOTICE: Normal command can't have arguments passed through, QBCore registered ones can have arguments passed through
-            icon = 'fas fa-example', -- This is the icon that will display next to this trigger option
+            icon = 'fas fa-stretcher', -- This is the icon that will display next to this trigger option
             label = 'Place on stretcher', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
             -- item = 'handcuffs', -- This is the item it has to check for, this option will only show up if the player has this item, this is OPTIONA
             job = 'ambulance', -- This is the job, this option won't show up if the player doesn't have this job, this can also be done with multiple jobs and grades, if you want multiple jobs you always need a grade with it: job = {["police"] = 0, ["ambulance"] = 2},
-            -- }, {
-            -- num = 2, -- This is the position number of your option in the list of options in the qb-target context menu (OPTIONAL)
-            -- type = "client", -- This specifies the type of event the target has to trigger on click, this can be "client", "server", "command" or "qbcommand", this is OPTIONAL and will only work if the event is also specified
-            -- event = "collar:detachCollar", -- This is the event it will trigger on click, this can be a client event, server event, command or qbcore registered command, NOTICE: Normal command can't have arguments passed through, QBCore registered ones can have arguments passed through
-            -- icon = 'fas fa-example', -- This is the icon that will display next to this trigger option
-            -- label = 'Remove cervical collar', -- This is the label of this option which you would be able to click on to trigger everything, this has to be a string
-            -- -- item = 'handcuffs', -- This is the item it has to check for, this option will only show up if the player has this item, this is OPTIONA
-            -- job = 'ambulance', -- This is the job, this option won't show up if the player doesn't have this job, this can also be done with multiple jobs and grades, if you want multiple jobs you always need a grade with it: job = {["police"] = 0, ["ambulance"] = 2},
             }
         },
         distance = 2.5, -- This is the distance for you to be at for the target to turn blue, this is in GTA units and has to be a float value
